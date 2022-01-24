0f123a87fa9004d1da92489d9efb24dd95b1b25c
Main Push Robot <robot@example.com>
2022-01-24 14:33:27 +0000

    Committing dev changes

---
+++ src/AS3935-Lightning-sensor-SOLDERED.cpp
@@ -0,0 +1,20 @@
+/**
+ **************************************************
+ *
+ * @file        AS3935-Lightning-sensor-SOLDERED.cpp
+ * @brief       Intentionaly blank.
+ *
+ *
+ * @copyright GNU General Public License v3.0
+ * @authors     Goran Juric @ soldered.com
+ ***************************************************/
+
+#include "AS3935-Lightning-sensor-SOLDERED.h"
+
+AS3935::AS3935()
+{
+}
+
+AS3935::AS3935(i2cAddress address) : SparkFun_AS3935(address )
+{
+}
